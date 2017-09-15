a4cc9f2ee14abe9ce6f9c21d475ec042ca0d28f1
王俊超 <shining-glory@qq.com>
2017-09-15 08:15:42 +0800

    commit

---
+++ 【第07章-MessagePack编解码】【02-MessagePack编码器和解码器开发】/src/main/java/com/netty/msgpack/MsgPackEncoder.java
@@ -0,0 +1,24 @@
+package com.netty.msgpack;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.codec.MessageToByteEncoder;
+
+/**
+ * Author: 王俊超
+ * Date: 2017-09-15 08:05
+ * Blog: http://blog.csdn.net/derrantcm
+ * Github: https://github.com/Wang-Jun-Chao
+ * All Rights Reserved !!!
+ */
+public class MsgPackEncoder extends MessageToByteEncoder<Object> {
+    @Override
+    protected void encode(ChannelHandlerContext ctx, Object msg, ByteBuf out) throws Exception {
+
+//        MessagePack msgPack = MessagePack.newDefaultPacker(out);
+//
+//        msgPack.
+//        out.writeBytes()
+
+    }
+}
