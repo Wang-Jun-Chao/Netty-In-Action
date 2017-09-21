d9bc8eb36379c71205f3ff10c1a6b30af4069f5d
王俊超 <shining-glory@qq.com>
2017-09-21 08:02:19 +0800

    commit

--- 【第08章-GoogleProtobuf编解码】【02-Netty的Protobuf服务端开发】/src/main/java/com/netty/bookorder/SubReqServer.java
+++ 【第08章-GoogleProtobuf编解码】【02-Netty的Protobuf服务端开发】/src/main/java/com/netty/bookorder/SubReqServer.java
@@ -1,5 +1,6 @@
 package com.netty.bookorder;
 
+import com.netty.protobuf.proto.SubscribeReqProto;
 import io.netty.bootstrap.ServerBootstrap;
 import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelInitializer;
@@ -8,6 +9,7 @@ import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.protobuf.ProtobufDecoder;
 import io.netty.handler.codec.protobuf.ProtobufEncoder;
 import io.netty.handler.codec.protobuf.ProtobufVarint32FrameDecoder;
 import io.netty.handler.logging.LogLevel;
@@ -34,8 +36,12 @@ public class SubReqServer {
                     .childHandler(new ChannelInitializer<SocketChannel>() {
                         @Override
                         public void initChannel(SocketChannel ch) {
+                            // 首先向ChannelPipeline 添加ProtobufVarint32FrameDecoder，它主要用于半包处
+                            // 理，随后继续添加ProtobufDecoder 解码器， 它的参数是com.google.protobuf.MessageLite，实
+                            // 际上就是要告诉ProtobufDecoder 需要解码的目标类是什么，否则仅仅从字节数组中是无
+                            // 法判断出要解码的目标类型信息的。
                             ch.pipeline().addLast(new ProtobufVarint32FrameDecoder());
-//                            ch.pipeline().addLast(new ProtobufDecoder(SubscribeReqProto.SubscribeReq.getDefaultInstance()));
+                            ch.pipeline().addLast(new ProtobufDecoder(SubscribeReqProto.SubscribeReq.getDefaultInstance()));
 //                            ch.pipeline().addLast(new ProtobufVarint32LengthFieldPrepender());
                             ch.pipeline().addLast(new ProtobufEncoder());
                             ch.pipeline().addLast(new SubReqServerHandler());
--- 【第08章-GoogleProtobuf编解码】【02-Netty的Protobuf服务端开发】/src/main/java/com/netty/bookorder/SubReqServerHandler.java
+++ 【第08章-GoogleProtobuf编解码】【02-Netty的Protobuf服务端开发】/src/main/java/com/netty/bookorder/SubReqServerHandler.java
@@ -15,8 +15,10 @@ import io.netty.channel.ChannelHandlerContext;
 public class SubReqServerHandler extends ChannelHandlerAdapter {
 
     @Override
-    public void channelRead(ChannelHandlerContext ctx, Object msg)
-            throws Exception {
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        // 由于ProtobutDecoder 已经对消息进行了自动解，码， 因此接收到的订购请求消息可以
+        // 直接使用。对用户名进行校验， 校验通过后构造应答消息返回给客户端， 由于使用了
+        // ProtobufEncoder ， 所以不需要对SubscribeRespProto.SubscribeResp进行手工编码。
         SubscribeReqProto.SubscribeReq req = (SubscribeReqProto.SubscribeReq) msg;
         if ("Wangjunchao".equalsIgnoreCase(req.getUserName())) {
             System.out.println("Service accept client subscribe req : [" + req.toString() + "]");
@@ -24,10 +26,9 @@ public class SubReqServerHandler extends ChannelHandlerAdapter {
         }
     }
 
-    private SubscribeRespProto.SubscribeResp resp(int subReqID) {
-        SubscribeRespProto.SubscribeResp.Builder builder = SubscribeRespProto.SubscribeResp
-                .newBuilder();
-        builder.setSubReqId(subReqID);
+    private SubscribeRespProto.SubscribeResp resp(int subReqId) {
+        SubscribeRespProto.SubscribeResp.Builder builder = SubscribeRespProto.SubscribeResp.newBuilder();
+        builder.setSubReqId(subReqId);
         builder.setRespCode(0);
         builder.setDesc("Netty book order succeed, 3 days later, sent to the designated address");
         return builder.build();
