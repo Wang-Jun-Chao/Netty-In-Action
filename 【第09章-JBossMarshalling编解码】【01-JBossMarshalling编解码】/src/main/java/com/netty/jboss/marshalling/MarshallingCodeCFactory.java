bf8a20a7c99f80470fcc5ff9135effdfc75dc0c1
王俊超 <shining-glory@qq.com>
2017-09-21 08:26:28 +0800

    commit

---
+++ 【第09章-JBossMarshalling编解码】【01-JBossMarshalling编解码】/src/main/java/com/netty/jboss/marshalling/MarshallingCodeCFactory.java
@@ -0,0 +1,20 @@
+package com.netty.jboss.marshalling;
+
+import io.netty.channel.ChannelHandler;
+
+/**
+ * Author: 王俊超
+ * Date: 2017-09-21 08:25
+ * Blog: http://blog.csdn.net/derrantcm
+ * Github: https://github.com/wang-jun-chao
+ * All Rights Reserved !!!
+ */
+public class MarshallingCodeCFactory {
+    public static ChannelHandler buildMarshallingDecoder() {
+        return null;
+    }
+
+    public static ChannelHandler buildMarshallingEncoder() {
+        return null;
+    }
+}
---
+++ 【第09章-JBossMarshalling编解码】【01-JBossMarshalling编解码】/src/main/java/com/netty/jboss/marshalling/SubReqServerHandler.java
@@ -0,0 +1,16 @@
+package com.netty.jboss.marshalling;
+
+import io.netty.channel.ChannelHandler;
+import io.netty.channel.ChannelHandlerAdapter;
+import io.netty.channel.ChannelHandlerInvoker;
+import io.netty.util.concurrent.EventExecutorGroup;
+
+/**
+ * Author: 王俊超
+ * Date: 2017-09-21 08:18
+ * Blog: http://blog.csdn.net/derrantcm
+ * Github: https://github.com/wang-jun-chao
+ * All Rights Reserved !!!
+ */
+public class SubReqServerHandler  extends ChannelHandlerAdapter {
+}
