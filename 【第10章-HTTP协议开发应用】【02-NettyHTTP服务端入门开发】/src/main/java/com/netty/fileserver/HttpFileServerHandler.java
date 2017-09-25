03dd540c3a6c9a1de1120d13433534713299f7eb
王俊超 <shining-glory@qq.com>
2017-09-25 08:19:01 +0800

    commit

--- 【第10章-HTTP协议开发应用】【02-NettyHTTP服务端入门开发】/src/main/java/com/netty/fileserver/HttpFileServerHandler.java
+++ 【第10章-HTTP协议开发应用】【02-NettyHTTP服务端入门开发】/src/main/java/com/netty/fileserver/HttpFileServerHandler.java
@@ -1,14 +1,17 @@
 package com.netty.fileserver;
 
 import io.netty.buffer.Unpooled;
-import io.netty.channel.ChannelFutureListener;
-import io.netty.channel.ChannelHandlerContext;
-import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.channel.*;
 import io.netty.handler.codec.http.*;
+import io.netty.handler.stream.ChunkedFile;
 import io.netty.util.CharsetUtil;
 
 import javax.activation.MimetypesFileTypeMap;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.RandomAccessFile;
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
 import java.util.regex.Pattern;
 
 /**
@@ -18,8 +21,9 @@ import java.util.regex.Pattern;
  * Github: https://github.com/wang-jun-chao
  * All Rights Reserved !!!
  */
-public class HttpFileServerHandler extends SimpleChannelInboundHandler<FullHttpRequest>{
+public class HttpFileServerHandler extends SimpleChannelInboundHandler<FullHttpRequest> {
     private static final Pattern INSECURE_URI = Pattern.compile(".*[<>&\"].*");
+    private static final Pattern ALLOWED_FILE_NAME = Pattern.compile("[A-Za-z0-9][-_A-Za-z0-9\\.]*");
     private final String url;
 
     public HttpFileServerHandler(String url) {
@@ -27,8 +31,128 @@ public class HttpFileServerHandler extends SimpleChannelInboundHandler<FullHttpR
     }
 
     @Override
-    protected void messageReceived(ChannelHandlerContext ctx, FullHttpRequest msg) throws Exception {
+    protected void messageReceived(ChannelHandlerContext ctx, FullHttpRequest request) throws Exception {
+        if (!request.decoderResult().isSuccess()) {
+            sendError(ctx, HttpResponseStatus.BAD_REQUEST);
+            return;
+        }
 
+        if (request.method() != HttpMethod.GET) {
+            sendError(ctx, HttpResponseStatus.METHOD_NOT_ALLOWED);
+            return;
+        }
+
+        final String uri = request.uri();
+        final String path = sanitizeUri(uri);
+        if (path == null) {
+            sendError(ctx, HttpResponseStatus.FORBIDDEN);
+            return;
+        }
+
+        File file = new File(path);
+        if (file.isHidden() || !file.exists()) {
+            sendError(ctx, HttpResponseStatus.NOT_FOUND);
+            return;
+        }
+
+        if (file.isDirectory()) {
+            if (uri.endsWith("/")) {
+                sendListing(ctx, file);
+            } else {
+                sendRedirect(ctx, uri + '/');
+            }
+            return;
+        }
+
+        if (!file.isFile()) {
+            sendError(ctx, HttpResponseStatus.FORBIDDEN);
+            return;
+        }
+
+        RandomAccessFile randomAccessFile = null;
+        try {
+            randomAccessFile = new RandomAccessFile(file, "r");
+        } catch (FileNotFoundException e) {
+            sendError(ctx, HttpResponseStatus.NOT_FOUND);
+            return;
+        }
+
+        long fileLength = randomAccessFile.length();
+        HttpResponse response = new DefaultFullHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.OK);
+        response.headers().addLong(HttpHeaderNames.CONTENT_LENGTH, fileLength);
+        setContentTypeHeader(response, file);
+
+        // 是否保活
+        if (HttpHeaderUtil.isKeepAlive(request)) {
+            response.headers().set(HttpHeaderNames.CONNECTION, HttpHeaderValues.KEEP_ALIVE);
+        }
+        ctx.write(response);
+
+        ChannelFuture sendFileFuture;
+        sendFileFuture = ctx.write(new ChunkedFile(randomAccessFile, 0, fileLength, 8192), ctx.newProgressivePromise());
+        sendFileFuture.addListener(new ChannelProgressiveFutureListener() {
+            @Override
+            public void operationProgressed(ChannelProgressiveFuture future, long progress, long total) throws Exception {
+                if (total < 0) { // 未知的总的处理数
+                    System.err.println("Transfer progress: " + progress);
+                } else {
+                    System.err.println("Transfer progress: " + progress + " / " + total);
+                }
+            }
+
+            @Override
+            public void operationComplete(ChannelProgressiveFuture future) throws Exception {
+                System.out.println("Transfer complete.");
+            }
+        });
+
+        ChannelFuture lastContentFuture = ctx.writeAndFlush(LastHttpContent.EMPTY_LAST_CONTENT);
+        if (HttpHeaderUtil.isKeepAlive(request)) {
+            lastContentFuture.addListener(ChannelFutureListener.CLOSE);
+        }
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        cause.printStackTrace();
+        if (ctx.channel().isActive()) {
+            sendError(ctx, HttpResponseStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    private void sendRedirect(ChannelHandlerContext ctx, String s) {
+
+    }
+
+    private void sendListing(ChannelHandlerContext ctx, File dir) {
+
+
+    }
+
+    // TODO
+    private String sanitizeUri(String uri) {
+        try {
+            uri = URLDecoder.decode(uri, "UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            try {
+                uri = URLDecoder.decode(uri, "ISO-8859-1");
+            } catch (UnsupportedEncodingException e1) {
+                throw new Error();
+            }
+        }
+        if (!uri.startsWith(url)) {
+            return null;
+        }
+        if (!uri.startsWith("/")) {
+            return null;
+        }
+        uri = uri.replace('/', File.separatorChar);
+        if (uri.contains(File.separator + '.')
+                || uri.contains('.' + File.separator) || uri.startsWith(".")
+                || uri.endsWith(".") || INSECURE_URI.matcher(uri).matches()) {
+            return null;
+        }
+        return System.getProperty("user.dir") + File.separator + uri;
     }
 
     private static void sendError(ChannelHandlerContext ctx, HttpResponseStatus status) {
