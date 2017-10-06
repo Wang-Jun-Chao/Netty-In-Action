08da6e6da1d931f156411b75d8148999e5b208ee
王俊超 <shining-glory@qq.com>
2017-10-07 07:39:35 +0800

    commit

---
+++ 【第12章-私有协议栈开发】【03-Netty协议栈开发】/src/main/java/com/netty/protocal/codec/MarshallingCodecFactory.java
@@ -0,0 +1,34 @@
+package com.netty.protocal.codec;
+
+import org.jboss.marshalling.*;
+
+import java.io.IOException;
+
+/**
+ * Author: 王俊超
+ * Date: 2017-10-07 07:34
+ * Blog: http://blog.csdn.net/derrantcm
+ * Github: https://github.com/wang-jun-chao
+ * All Rights Reserved !!!
+ */
+public class MarshallingCodecFactory {
+    /**
+     * 创建Jboss Marshaller
+     */
+    protected static Marshaller buildMarshalling() throws IOException {
+        final MarshallerFactory marshallerFactory = Marshalling.getProvidedMarshallerFactory("serial");
+        final MarshallingConfiguration configuration = new MarshallingConfiguration();
+        configuration.setVersion(5);
+        return marshallerFactory.createMarshaller(configuration);
+    }
+
+    /**
+     * 创建Jboss Unmarshaller
+     */
+    protected static Unmarshaller buildUnMarshalling() throws IOException {
+        final MarshallerFactory marshallerFactory = Marshalling.getProvidedMarshallerFactory("serial");
+        final MarshallingConfiguration configuration = new MarshallingConfiguration();
+        configuration.setVersion(5);
+        return marshallerFactory.createUnmarshaller(configuration);
+    }
+}
---
+++ 【第12章-私有协议栈开发】【03-Netty协议栈开发】/src/main/java/com/netty/protocal/codec/MarshallingDecoder.java
@@ -0,0 +1,43 @@
+package com.netty.protocal.codec;
+
+import io.netty.buffer.ByteBuf;
+import org.jboss.marshalling.ByteInput;
+import org.jboss.marshalling.Unmarshaller;
+
+import java.io.IOException;
+
+/**
+ * Author: 王俊超
+ * Date: 2017-10-07 07:35
+ * Blog: http://blog.csdn.net/derrantcm
+ * Github: https://github.com/wang-jun-chao
+ * All Rights Reserved !!!
+ */
+public class MarshallingDecoder {
+    private final Unmarshaller unmarshaller;
+
+    /**
+     * 创建一个解码器，解码的对象的最大大小是1048576个字节，如果解码对象大于1048576字节，
+     * 则会抛出StreamCorruptedException异常
+     *
+     * @throws IOException
+     */
+    public MarshallingDecoder() throws IOException {
+        unmarshaller = MarshallingCodecFactory.buildUnMarshalling();
+    }
+
+    protected Object decode(ByteBuf in) throws Exception {
+        int objectSize = in.readInt();
+        ByteBuf buf = in.slice(in.readerIndex(), objectSize);
+        ByteInput input = new ChannelBufferByteInput(buf);
+        try {
+            unmarshaller.start(input);
+            Object obj = unmarshaller.readObject();
+            unmarshaller.finish();
+            in.readerIndex(in.readerIndex() + objectSize);
+            return obj;
+        } finally {
+            unmarshaller.close();
+        }
+    }
+}
